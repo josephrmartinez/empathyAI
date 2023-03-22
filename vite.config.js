c1487eb437dca7c1ac00021037ddd9062d3c53ee
Joseph Martinez <joseph.r.martinez@gmail.com>
2023-03-21 20:55:37 -0700

    Removed firebase hosting, ran npm run build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    minify: 'esbuild',
+    target: "esnext"
+  },
   plugins: [react(), svgr()],
 })
